bc8980f87ea23ad7a729219e3811900b820d3773
pmedam <142865611+pmedam@users.noreply.github.com>
2023-12-04 00:27:03 -0600

    Work In Progress

---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div className='w-full md:h-screen p-2 flex items-center py-16'>
+            <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+                <div className='col-span-2'>
+                    <p>About</p>
+                    <h2>Who I Am</h2>
+                    <p></p>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ components/Main.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const Main: React.FC = () => {
+    return (
+        <div className='w-full h-screen text-center'>
+            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                <div>
+                    <p className='uppercase text-sm tracking-widest text-gray-600'>
+                        Let's Build Something Together
+                    </p>
+                    <h1 className='py-4 text-gray-600'>
+                        Hi, I'm <span className='text-[#5651e5]'>
+                            Pradeep Kumar Medam</span>
+                    </h1>
+                    <h2 className='py-2 text-gray-600'>
+                        I'm a Front End Web Developer
+                    </h2>
+                    <p className='py-4 text-gray-700 max-w-[70%] m-auto'>I'm a front-end developer specializing in building responsive web applications.
+                        Currently, I'm focused on building responsive full-stack web applications.
+                        I'm passionate about learning and creating.
+                    </p>
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaLinkedinIn />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaGithub />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineMail />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <BsFillPersonLinesFill />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
